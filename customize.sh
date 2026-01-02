9d1009c1926f4e2ac016744566384875d09fe5da
suangnet <aswand1@ymail.com>
2026-01-02 11:47:36 +0800

    Update typo

--- customize.sh
+++ customize.sh
@@ -51,9 +51,6 @@ if [ -f "$MODPATH/service.d/tailscale_service.sh" ]; then
     rmdir $MODPATH/service.d
 fi
 
-ui_print "- Permission have been set"
-ui_print " "
-ui_print "[100] Finish!"
-ui_print "Reboot to start service"
-ui_print "Support: https://t.me/suangnet"
-ui_print " "
+ui_print "- Installation complete!"
+ui_print "- Reboot to start tailscale service."
+ui_print "- Support: https://t.me/suangnet"
